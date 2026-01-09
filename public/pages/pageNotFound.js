919474658a69ab20b40a489880951a76c4bd3718
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-01-09 09:27:43 +0800

    Implemented new redicrection

--- public/pages/pageNotFound.js
+++ public/pages/pageNotFound.js
@@ -1,8 +1,16 @@
 import MainOnlyLayout from '../layouts/mainOnly.js';
 import Main from '../components/pageNotFound/main.js';
+import Events from '../components/pageNotFound/event.js';
 
-export default function PageNotFound() {
-    const { main } = MainOnlyLayout(this.root);
+export default class PageNotFound {
+    constructor(root) {
+        this.root = root;
+    }
 
-    Main(main);
+    render() {
+        const { main } = MainOnlyLayout(this.root);
+
+        Main(main);
+        Events(this.root);
+    }
 }
